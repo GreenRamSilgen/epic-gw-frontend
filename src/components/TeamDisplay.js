714292e59ae03e1a4da829d1feb2cdfae95109d0
GreenRamSilgen <ps3iswow@gmail.com>
2020-09-13 20:26:51 -0700

    fetching from local backend success.

--- src/components/TeamDisplay.js
+++ src/components/TeamDisplay.js
@@ -10,11 +10,18 @@ class TeamDisplay extends React.Component {
     super(props);
     
     this.state={
+      loading: true,
       showUpdateForm: false,
+      heroDatas: null,
     }
 
     this.setUpdateFormOn = this.setUpdateFormOn.bind(this);
     this.setUpdateFormOff = this.setUpdateFormOff.bind(this);
+
+
+    //this.props.match.params.name
+
+    
   }
 
   setUpdateFormOn(){
@@ -27,10 +34,35 @@ class TeamDisplay extends React.Component {
       showUpdateForm: false
     })
   }
+
+  async componentDidMount(){
+    const url = 'http://localhost:3001/leftTowerDisplay/tower1';
+    //`/${this.props.match.params.name}/${this.props.match.params.towerNum}`
+    const response = await fetch(url);
+    const data = await response.json();
+
+    this.setState({
+      loading: false,
+      heroDatas: data
+    })
+    //.then(res=> res.text())
+    //.then(finalResponse =>{
+     // console.log(finalResponse);
+    //})
+    //.catch(e => {
+     // console.log(e);
+    //});
+  }
   render() {
+    console.log(this.state.heroDatas);
     return (
       <div>
         {(this.state.showUpdateForm) ? <UpdateForm setUpdateFormOff={this.setUpdateFormOff}/> : null}
+        {this.state.loading || !this.state.heroDatas ? (
+          <div> loading...</div>
+        ) :  (
+        <div> {this.state.heroDatas[0].name}</div>
+        )}
         <div className="team-container">
           <div className="team-name-options">
             <div className="teamNum">
@@ -50,9 +82,9 @@ class TeamDisplay extends React.Component {
                 </div>
               </div>
             </div>
-            <CharDisplay setUpdateFormOn={this.setUpdateFormOn}/>
-            <CharDisplay setUpdateFormOn={this.setUpdateFormOn}/>
-            <CharDisplay setUpdateFormOn={this.setUpdateFormOn}/>
+            <CharDisplay heroData={(this.state.heroDatas !== null) ? this.state.heroDatas[0] : null } setUpdateFormOn={this.setUpdateFormOn}/>
+            <CharDisplay heroData={(this.state.heroDatas !== null) ? this.state.heroDatas[1] : null } setUpdateFormOn={this.setUpdateFormOn}/>
+            <CharDisplay heroData={(this.state.heroDatas !== null) ? this.state.heroDatas[2] : null } setUpdateFormOn={this.setUpdateFormOn}/>
           </div>
 
           <div className="team-name-options">
@@ -73,9 +105,9 @@ class TeamDisplay extends React.Component {
                 </div>
               </div>
             </div>
-            <CharDisplay setUpdateFormOn={this.setUpdateFormOn}/>
-            <CharDisplay setUpdateFormOn={this.setUpdateFormOn}/>
-            <CharDisplay setUpdateFormOn={this.setUpdateFormOn}/>
+            <CharDisplay heroData={(this.state.heroDatas !== null) ? this.state.heroDatas[3] : null } setUpdateFormOn={this.setUpdateFormOn}/>
+            <CharDisplay heroData={(this.state.heroDatas !== null) ? this.state.heroDatas[4] : null } setUpdateFormOn={this.setUpdateFormOn}/>
+            <CharDisplay heroData={(this.state.heroDatas !== null) ? this.state.heroDatas[5] : null } setUpdateFormOn={this.setUpdateFormOn}/>
           </div>
         </div>
       </div>
